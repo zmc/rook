ed6d880c7626ac89062bb3ee2a31c18b7c061542
Sébastien Han <seb@redhat.com>
2020-01-07 10:36:18 +0100

    ceph: add option to continue on unclean PGs
    A new CRD option `continueUpgradeAfterChecksEvenIfNotHealthy` is added.
    When upgrading, Rook goes OSD by OSD and then waits for PGs to be clean
    before proceeding to the next OSD. Currently, Rook waits for 5 hours but
    there might be circumstances where PGs need more time to settle.
    Thus setting `continueUpgradeAfterChecksEvenIfNotHealthy` to true will
    pursue the upgrade process, even if PGs are not 100% active+clean.
    
    Closes: https://bugzilla.redhat.com/show_bug.cgi?id=1786029
    Signed-off-by: Sébastien Han <seb@redhat.com>
    (cherry picked from commit 05aa639317884c7ff29ddfc2698ec52f8c1ce588)

--- pkg/operator/ceph/cluster/mon/spec.go
+++ pkg/operator/ceph/cluster/mon/spec.go
@@ -344,7 +344,7 @@ func (c *Cluster) makeMonDaemonContainer(monConfig *monConfig) v1.Container {
 }
 
 // UpdateCephDeploymentAndWait verifies a deployment can be stopped or continued
-func UpdateCephDeploymentAndWait(context *clusterd.Context, deployment *apps.Deployment, namespace, daemonType, daemonName string, cephVersion cephver.CephVersion, isUpgrade, skipUpgradeChecks bool) error {
+func UpdateCephDeploymentAndWait(context *clusterd.Context, deployment *apps.Deployment, namespace, daemonType, daemonName string, cephVersion cephver.CephVersion, isUpgrade, skipUpgradeChecks, continueUpgradeAfterChecksEvenIfNotHealthy bool) error {
 
 	callback := func(action string) error {
 		if !isUpgrade {
@@ -362,14 +362,24 @@ func UpdateCephDeploymentAndWait(context *clusterd.Context, deployment *apps.Dep
 		if action == "stop" {
 			err := client.OkToStop(context, namespace, deployment.Name, daemonType, daemonName, cephVersion)
 			if err != nil {
-				return fmt.Errorf("failed to check if we can %s the deployment %s: %+v", action, deployment.Name, err)
+				if continueUpgradeAfterChecksEvenIfNotHealthy {
+					logger.Infof("The %s daemon %s is not ok-to-stop but 'continueUpgradeAfterChecksEvenIfNotHealthy' is true, so proceeding to stop...", daemonType, daemonName)
+					return nil
+				} else {
+					return fmt.Errorf("failed to check if we can %s the deployment %s: %+v", action, deployment.Name, err)
+				}
 			}
 		}
 
 		if action == "continue" {
 			err := client.OkToContinue(context, namespace, deployment.Name, daemonType, daemonName)
 			if err != nil {
-				return fmt.Errorf("failed to check if we can %s the deployment %s: %+v", action, deployment.Name, err)
+				if continueUpgradeAfterChecksEvenIfNotHealthy {
+					logger.Infof("The %s daemon %s is not ok-to-stop but 'continueUpgradeAfterChecksEvenIfNotHealthy' is true, so continuing...", daemonType, daemonName)
+					return nil
+				} else {
+					return fmt.Errorf("failed to check if we can %s the deployment %s: %+v", action, deployment.Name, err)
+				}
 			}
 		}
 
