bcd2d016fd148c38b07ee7af05f5e9937dd773c1
Travis Nielsen <tnielsen@redhat.com>
2021-03-18 16:37:01 -0600

    ceph: set active mgr label on mgr services
    Other resources such as the service monitor need to match
    the labels on the services to the active mgr, thus the active
    mgr name is now set on the labels for the metrics service
    and dashboard service.
    
    Signed-off-by: Travis Nielsen <tnielsen@redhat.com>
    (cherry picked from commit c78e6a9e20d9f7c3a78b5891f0d390c48edc401a)

--- pkg/operator/ceph/cluster/mgr/spec.go
+++ pkg/operator/ceph/cluster/mgr/spec.go
@@ -291,7 +291,7 @@ func getDefaultMgrLivenessProbe() *v1.Probe {
 
 // MakeMetricsService generates the Kubernetes service object for the monitoring service
 func (c *Cluster) MakeMetricsService(name, activeDaemon, servicePortMetricName string) *v1.Service {
-	labels := controller.AppLabels(AppName, c.clusterInfo.Namespace)
+	labels := c.selectorLabels(activeDaemon)
 
 	svc := &v1.Service{
 		ObjectMeta: metav1.ObjectMeta{
@@ -313,7 +313,7 @@ func (c *Cluster) MakeMetricsService(name, activeDaemon, servicePortMetricName s
 
 	// If the cluster is external we don't need to add the selector
 	if name != ExternalMgrAppName {
-		svc.Spec.Selector = c.selectorLabels(activeDaemon)
+		svc.Spec.Selector = labels
 	}
 
 	k8sutil.SetOwnerRef(&svc.ObjectMeta, &c.clusterInfo.OwnerRef)
@@ -321,9 +321,7 @@ func (c *Cluster) MakeMetricsService(name, activeDaemon, servicePortMetricName s
 }
 
 func (c *Cluster) makeDashboardService(name, activeDaemon string) *v1.Service {
-	labels := controller.AppLabels(AppName, c.clusterInfo.Namespace)
-	selectorLabels := controller.AppLabels(AppName, c.clusterInfo.Namespace)
-	selectorLabels[controller.DaemonIDLabel] = activeDaemon
+	labels := c.selectorLabels(activeDaemon)
 
 	portName := "https-dashboard"
 	if !c.spec.Dashboard.SSL {
@@ -336,7 +334,7 @@ func (c *Cluster) makeDashboardService(name, activeDaemon string) *v1.Service {
 			Labels:    labels,
 		},
 		Spec: v1.ServiceSpec{
-			Selector: selectorLabels,
+			Selector: labels,
 			Type:     v1.ServiceTypeClusterIP,
 			Ports: []v1.ServicePort{
 				{
