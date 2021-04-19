bf1670d7d9251fb691bb4c000ff48b8a1e89f5ff
Sébastien Han <seb@redhat.com>
2021-04-19 16:07:13 +0200

    ceph: always apply config flags for mds and rgw
    We now always set the config flags on reconcile. Previously we were
    looking for non-existing rgw or mds which means that on upgrade we would
    never set those flags. However, we want to set those flags on "old"
    cluster that got upgraded.
    
    Signed-off-by: Sébastien Han <seb@redhat.com>
    (cherry picked from commit ca1cf6672e80cc1bab764d2d29751cdb4906e89d)

--- pkg/operator/ceph/file/filesystem_test.go
+++ pkg/operator/ceph/file/filesystem_test.go
@@ -19,6 +19,7 @@ package file
 import (
 	"context"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
@@ -203,8 +204,10 @@ func TestCreateFilesystem(t *testing.T) {
 				return "", nil
 			} else if reflect.DeepEqual(args[0:6], []string{"osd", "pool", "set", "myfs-data1", "size", "1"}) {
 				return "", nil
+			} else if args[0] == "config" && args[1] == "set" {
+				return "", nil
 			}
-			assert.Fail(t, "Unexpected command")
+			assert.Fail(t, fmt.Sprintf("Unexpected command: %v", args))
 			return "", nil
 		},
 	}
